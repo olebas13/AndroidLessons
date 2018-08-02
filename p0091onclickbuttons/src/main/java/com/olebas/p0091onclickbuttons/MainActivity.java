a8be01b932d0c914e0665a59e4260bb2a8796b5e
olebas13 <o.nevoyt13@gmail.com>
2018-08-02 11:03:08 +0300

    init commit

---
+++ p0091onclickbuttons/src/main/java/com/olebas/p0091onclickbuttons/MainActivity.java
@@ -0,0 +1,38 @@
+package com.olebas.p0091onclickbuttons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvOut;
+    private Button btnOk;
+    private Button btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvOut.setText("Нажата кнопка ОК");
+            }
+        });
+
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvOut.setText("Нажата кнопка Cancel");
+            }
+        });
+
+    }
+}
