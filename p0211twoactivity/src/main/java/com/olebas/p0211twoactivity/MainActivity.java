ce0d40f1901d0f9e4ebc309144354084caede9f6
olebas13 <o.nevoyt13@gmail.com>
2018-08-03 14:20:04 +0300

    finish lesson 21, add one more activity

---
+++ p0211twoactivity/src/main/java/com/olebas/p0211twoactivity/MainActivity.java
@@ -0,0 +1,33 @@
+package com.olebas.p0211twoactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnActTwo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnActTwo:
+                Intent intent = new Intent(this, ActivityTwo.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}
