21cb88ed64139d1048255f623cd04a6d6bf01458
olebas13 <o.nevoyt13@gmail.com>
2018-08-03 16:56:34 +0300

    finish lesson 26 IntentFilter

---
+++ p0261intentfilter/src/main/java/com/olebas/p0261intentfilter/MainActivity.java
@@ -0,0 +1,39 @@
+package com.olebas.p0261intentfilter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnTime = (Button) findViewById(R.id.btnTime);
+        Button btnDate = (Button) findViewById(R.id.btnDate);
+
+        btnTime.setOnClickListener(this);
+        btnDate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+
+        switch (view.getId()) {
+            case R.id.btnTime:
+                intent = new Intent("com.olebas.intent.action.showtime");
+                startActivity(intent);
+                break;
+            case R.id.btnDate:
+                intent = new Intent("com.olebas.intent.action.showdate");
+                startActivity(intent);
+                break;
+        }
+    }
+}
