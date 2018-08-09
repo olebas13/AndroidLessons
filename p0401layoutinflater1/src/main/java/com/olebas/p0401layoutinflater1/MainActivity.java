36416118bb2bd446375fc3acf891f631e6924526
olebas13 <o.nevoyt13@gmail.com>
2018-08-09 17:36:46 +0300

    finish lesson 41

---
+++ p0401layoutinflater1/src/main/java/com/olebas/p0401layoutinflater1/MainActivity.java
@@ -0,0 +1,34 @@
+package com.olebas.p0401layoutinflater1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import static android.view.ViewGroup.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LayoutInflater ltInflater = getLayoutInflater();
+        View view = ltInflater.inflate(R.layout.text, null, false);
+        LayoutParams lp = view.getLayoutParams();
+
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+        linLayout.addView(view);
+
+        Log.d(LOG_TAG, "Class of view: " + view.getClass().toString());
+        Log.d(LOG_TAG, "LayoutParams of view is null: " + (lp == null));
+        Log.d(LOG_TAG, "Text of view: " + ((TextView) view).getText());
+    }
+}
