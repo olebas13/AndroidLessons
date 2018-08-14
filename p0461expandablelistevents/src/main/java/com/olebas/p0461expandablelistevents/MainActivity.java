64a8a74cbede3bcedaace502a1bc6503759602f7
olebas13 <o.nevoyt13@gmail.com>
2018-08-14 17:10:45 +0300

    finish lesson 46

--- p0461expandablelistevents/src/main/java/com/olebas/p0461expandablelistevents/MainActivity.java
+++ p0461expandablelistevents/src/main/java/com/olebas/p0461expandablelistevents/MainActivity.java
@@ -1,13 +1,77 @@
 package com.olebas.p0461expandablelistevents;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
+import android.widget.TextView;
+
+import static android.widget.ExpandableListView.OnChildClickListener;
+import static android.widget.ExpandableListView.OnGroupClickListener;
+import static android.widget.ExpandableListView.OnGroupCollapseListener;
+import static android.widget.ExpandableListView.OnGroupExpandListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    ExpandableListView elvMain;
+    AdapterHelper ah;
+    SimpleExpandableListAdapter adapter;
+    TextView tvInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvInfo = (TextView) findViewById(R.id.tvInfo);
+
+        ah = new AdapterHelper(this);
+        adapter = ah.getAdapter();
+
+        elvMain = (ExpandableListView) findViewById(R.id.elvMain);
+        elvMain.setAdapter(adapter);
+
+        elvMain.setOnChildClickListener(new OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                Log.d(LOG_TAG, "onChildClick groupPosition = " + groupPosition + " childPosition = " + childPosition + " id = " + id);
+                tvInfo.setText(ah.getGroupChildText(groupPosition, childPosition));
+                return false;
+            }
+        });
+
+        elvMain.setOnGroupClickListener(new OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                Log.d(LOG_TAG, "onGroupClick groupPosition = " + groupPosition +
+                        " id = " + id);
+//                if (groupPosition == 1) {
+//                    return true;
+//                }
+                return false;
+            }
+        });
+
+        elvMain.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Log.d(LOG_TAG, "onGroupCollapse groupPosition = " + groupPosition);
+                tvInfo.setText("Свернули " + ah.getGroupText(groupPosition));
+            }
+        });
+
+        elvMain.setOnGroupExpandListener(new OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Log.d(LOG_TAG, "onGroupExpand groupPosition = " + groupPosition);
+                tvInfo.setText("Развернули " + ah.getGroupText(groupPosition));
+            }
+        });
+
+//        elvMain.expandGroup(2);
     }
 }
