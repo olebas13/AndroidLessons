4d79875ef1f5559c1dc79be8fd9efe55dea5dfc2
olebas13 <o.nevoyt13@gmail.com>
2018-08-16 14:00:41 +0300

    finish lesson 51

---
+++ p0511simpleadapterdata/src/main/java/com/olebas/p0511simpleadapterdata/MainActivity.java
@@ -0,0 +1,77 @@
+package com.olebas.p0511simpleadapterdata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.view.ContextMenu.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CM_DELETE_ID = 1;
+
+    final String ATTRIBUTE_NAME_TEXT = "text";
+    final String ATTRIBUTE_NAME_IMG = "image";
+
+    ListView lvSimple;
+    SimpleAdapter sAdapter;
+    ArrayList<Map<String, Object>> data;
+    Map<String, Object> m;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        data = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < 5; i++) {
+            m = new HashMap<String, Object>();
+            m.put(ATTRIBUTE_NAME_TEXT, "sometext " + i);
+            m.put(ATTRIBUTE_NAME_IMG, R.mipmap.ic_launcher);
+            data.add(m);
+        }
+
+        String[] from = {ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_IMG};
+        int[] to = {R.id.tvText, R.id.ivImg};
+
+        sAdapter = new SimpleAdapter(this, data, R.layout.item, from, to);
+
+        lvSimple = (ListView) findViewById(R.id.lvSimple);
+        lvSimple.setAdapter(sAdapter);
+        registerForContextMenu(lvSimple);
+    }
+
+    public void onButtonClick(View v) {
+        m = new HashMap<String, Object>();
+        m.put(ATTRIBUTE_NAME_TEXT, "sometext " + (data.size() + 1));
+        m.put(ATTRIBUTE_NAME_IMG, R.mipmap.ic_launcher);
+        data.add(m);
+        sAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, CM_DELETE_ID, 0, "Удалить запись");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == CM_DELETE_ID) {
+            AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            data.remove(acmi.position);
+            sAdapter.notifyDataSetChanged();
+            return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+}
