30b74ec2310ffa35c3cceae46e730a95448cff0f
olebas13 <o.nevoyt13@gmail.com>
2018-08-20 18:05:50 +0300

    lesson 54

---
+++ p0541customadapter/src/main/java/com/olebas/p0541customadapter/BoxAdapter.java
@@ -0,0 +1,66 @@
+package com.olebas.p0541customadapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class BoxAdapter extends BaseAdapter {
+
+    Context ctx;
+    LayoutInflater lInflater;
+    ArrayList<Product> objects;
+
+    BoxAdapter(Context context, ArrayList<Product> products) {
+        ctx = context;
+        objects = products;
+        lInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return objects.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return objects.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = lInflater.inflate(R.layout.item, parent, false);
+        }
+
+        Product p = getProduct(position);
+
+        ((TextView) view.findViewById(R.id.tvDescr)).setText(p.name);
+        ((TextView) view.findViewById(R.id.tvPrice)).setText(p.price);
+        ((ImageView) view.findViewById(R.id.ivImage)).setImageResource(p.image);
+
+        CheckBox cbBuy = (CheckBox) view.findViewById(R.id.cbBox);
+        cbBuy.setOnCheckedChangeListener(myCheckChangeList);
+        cbBuy.setTag(position);
+        cbBuy.setChecked(p.box);
+
+        return view;
+    }
+
+    private Product getProduct(int position) {
+    }
+
+}
